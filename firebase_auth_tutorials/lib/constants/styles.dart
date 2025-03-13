59b47a36726276986f67ff0cb854e94cef9f013d
nadeeja0 <nadeejakeerthirathna605@gmail.com>
2025-03-13 17:17:59 +0530

    Sign in page
    frontend of Sign in page

---
+++ firebase_auth_tutorials/lib/constants/styles.dart
@@ -0,0 +1,5 @@
+
+import 'package:firebase_auth_tutorials/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle descriptionStyle = TextStyle(fontSize: 12,color: textLight,fontWeight: FontWeight.w400);
--- firebase_auth_tutorials/lib/screens/authentication/login.dart
+++ firebase_auth_tutorials/lib/screens/authentication/login.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth_tutorials/constants/colors.dart';
+import 'package:firebase_auth_tutorials/constants/styles.dart';
 import 'package:firebase_auth_tutorials/models/UserModel.dart';
 import 'package:firebase_auth_tutorials/services/auth.dart';
 import 'package:flutter/material.dart';
@@ -15,25 +17,121 @@ class _SignInState extends State<SignIn> {
 
   //references for the AuthServices class
   final AuthServices _auth = AuthServices();
+  final _formkeyy =GlobalKey<FormState>();
+  String email="";
+  String password="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgBlack,
       appBar: AppBar(
-        title: Text("SIGN IN"),
+        backgroundColor: bgBlack,
+        title: Text("SIGN IN",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
       ),
-      body: ElevatedButton(
-          onPressed: () async{
-            dynamic resulut= await _auth.signInAnonymouslyy();
-            if(resulut == null){
-              print("error in sign in anon");
+      body:SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Text(" Sign-In Page allows users to access their accounts securely by entering their credentials, such as email/username and password. It typically includes options for password recovery and social media login for convenience. A well-designed sign-in page ensures a seamless and secure user experience.",
+                style: descriptionStyle,textAlign:TextAlign.left ,),
+              Center(child: Image.asset("assets/man.png",scale: 5,)),
+              Form(
+                key: _formkeyy,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextFormField(
+                        validator: (Evalue)=>Evalue?.isEmpty == true ? "enter a valid Email":null,
+        
+                        onChanged: (Evalue){
+                          setState(() {
+                            email=Evalue;
+                          });
+                        }
+        
+                      ),
+                      TextFormField(
+                          validator: (Pvalue)=>Pvalue!.length<6 == true ? "enter a valid Password":null,
+        
+                          onChanged: (Pvalue){
+                            setState(() {
+                              password=Pvalue;
+                            });
+                          }
+        
+                      ),
+                      SizedBox(height: 30,),
+                      Text("Login with social Accounts",style: descriptionStyle,),
+                      SizedBox(height: 10,),
+                      GestureDetector(
+                        onTap: (){},
+                          child: Image.asset("assets/google.png",scale: 20,)),
+                      SizedBox(height: 10,),
+                      Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Do not have an account?",style: descriptionStyle),
+                          GestureDetector(
+                            onTap: (){},
+                              child: Text("Register",style: TextStyle(fontWeight: FontWeight.w600,color: mainBlue),))
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      GestureDetector(
+                        onTap: (){},
+                        child: Container(
+                          height: 40,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: bgBlack,
+                            border: Border.all(width: 2,color: mainYellow),
 
-            }else{
-              print("signed in anon");
-              print(resulut.uid);
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Center(child: Text("LOGIN",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500),)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: (){},
+                        child: Container(
+                          height: 40,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: bgBlack,
+                            border: Border.all(width: 2,color: mainYellow),
 
-            }
-          },
-          child: Text("Sign in anonymously")),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Center(child: Text("LOGIN AS GUEST",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500),)),
+                        ),
+                      ),
+
+        
+                    ],
+                  )
+              )
+        
+            ],
+          ),
+        ),
+      )
     );
   }
 }
+
+// ElevatedButton(
+// onPressed: () async{
+// dynamic resulut= await _auth.signInAnonymouslyy();
+// if(resulut == null){
+// print("error in sign in anon");
+//
+// }else{
+// print("signed in anon");
+// print(resulut.uid);
+//
+// }
+// },
+// child: Text("Sign in anonymously")),
